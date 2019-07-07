c94506b3452e458009f41e20a38e2926529d93d9
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-07 22:05:33 +0200

    Add project files.

---
+++ DisplayDate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to displays the Day properties (year, month, day, hour, minute, second, millisecond)");
+
+            System.DateTime currentMoment = new DateTime(2019, 6, 15, 11, 41, 50, 59);
+            Console.WriteLine("year: " + currentMoment.Year);
+            Console.WriteLine("month: "+ currentMoment.Month);
+            Console.WriteLine("day: "+ currentMoment.Day);
+            Console.WriteLine("hour: "+ currentMoment.Hour);
+            Console.WriteLine("minute: "+ currentMoment.Minute);
+            Console.WriteLine("second: "+ currentMoment.Second);
+            Console.WriteLine("milisecond: " + currentMoment.Millisecond);
+
+            Console.ReadKey();
+        }
+    }
+}
